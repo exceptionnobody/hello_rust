8cec4708761e5b59c3d3db00588877237d780030
Francesco Galazzo <72932124+exceptionnobody@users.noreply.github.com>
2023-03-06 14:59:28 +0100

    test committ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vet = Vec::<i32>::new();
+    vet.push(1);
+    vet.push(2);
+
+    println!("before clearing vet: {:?}", vet);
+    vet.clear();
+    println!("after clearing vet: {:?}", vet);
 }
